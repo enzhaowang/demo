8753af90084c0c6643b023483c509e991d5e1feb
Wang Enzhao <enzhao.wang@smartmore.com>
2022-03-09 18:17:09 +0800

    提交分支

---
+++ git-test/src/main/java/com/wang/test/Master.java
@@ -0,0 +1,13 @@
+package com.wang.test;
+
+/**
+ * @program: git-test
+ * @description: masterclass
+ * @author: Wang Enzhao
+ * @create: 2022-03-09 18:15
+ **/
+public class Master {
+    public static void main(String[] args) {
+        System.out.println("this is a master branch");
+    }
+}

ae7b3644c0b882f76f992fefeafbe30e95b8c4b5
Wang Enzhao <enzhao.wang@smartmore.com>
2022-03-09 18:19:24 +0800

    dev 分支上面提交了一个class

---
+++ git-test/src/main/java/com/wang/test/Dev.java
@@ -0,0 +1,13 @@
+package com.wang.test;
+
+/**
+ * @program: git-test
+ * @description: dev class
+ * @author: Wang Enzhao
+ * @create: 2022-03-09 18:18
+ **/
+public class Dev {
+    public static void main(String[] args) {
+        System.out.println("this is a dev class");
+    }
+}
